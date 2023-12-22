7e9d1c8b6cb85dbc6287f1e89f0c687b743b94c7
Malekmontanaimam <malekimam123@gmail.com>
2023-12-22 12:44:50 +0300

    reloadmalek

--- database/migrations/2023_11_26_102014_create_products_table.php
+++ database/migrations/2023_11_26_102014_create_products_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('category_id')->constrained('categories')
             ->cascadeOnDelete('cascade');
+          //  $table->enum()
             $table->string('scientific_name');
             $table->string('commercial_name');
             $table->string('company');
